af60d8215d74b4a733763c15b308cbfe4091f92b
naya-beene <104564886+naya-beene@users.noreply.github.com>
2023-04-26 17:43:11 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 96 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,11 @@
+T = int(input())
+
+for _ in range(T) :
+    C = int(input())
+    res = []
+
+    for i in [25, 10, 5, 1] :
+        res.append(C // i)	# 몫이 개수
+        C = C % i	# 나머지는 다시 C에 저장
+        
+    print(*res)	# 한 줄 출력
